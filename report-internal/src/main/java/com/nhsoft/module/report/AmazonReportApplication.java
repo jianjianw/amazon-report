22b564c56746ba96da367cc8275687be81a565c2
王磊 <nhsoft.wl@hotmail.com>
2017-10-25 13:34:23 +0800

    add

--- report-internal/src/main/java/com/nhsoft/module/report/AmazonReportApplication.java
+++ report-internal/src/main/java/com/nhsoft/module/report/AmazonReportApplication.java
@@ -4,13 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
-@ImportResource("classpath:dubbo.xml")
+@ImportResource(locations={"classpath:dubbo.xml"})
 @SpringBootApplication(exclude={JpaRepositoriesAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-public class AmazonReportApplication {
+public class AmazonReportApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmazonReportApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder application) {
+		return application.sources(AmazonReportApplication.class);
 	}
 }
