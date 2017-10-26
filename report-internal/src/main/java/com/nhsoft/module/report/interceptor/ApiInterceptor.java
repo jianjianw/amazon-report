4b843991be3ebbc1743a2d7a7ddbc0170dbc2dbb
王磊 <nhsoft.wl@hotmail.com>
2017-10-26 11:28:53 +0800

    add

--- report-internal/src/main/java/com/nhsoft/module/report/interceptor/ApiInterceptor.java
+++ report-internal/src/main/java/com/nhsoft/module/report/interceptor/ApiInterceptor.java
@@ -1,34 +1,46 @@
 package com.nhsoft.module.report.interceptor;
 
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletWebRequest;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-public class ApiInterceptor implements HandlerInterceptor {
+@Aspect
+@Configuration
+public class ApiInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(ApiInterceptor.class);
-    private long  preTime;
-    String url;
-    @Override
-    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
-        url = httpServletRequest.getRequestURL().toString();
-        preTime = System.currentTimeMillis();
-        return true;
+
+    @Pointcut("execution(* com.nhsoft.module.report.api.*.*(..))")
+    public void api() {
     }
 
-    @Override
-    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
 
-    }
+    @Around(value="ApiInterceptor.api()")
+    public void printLog(ProceedingJoinPoint point){
 
-    @Override
-    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        ServletWebRequest servletContainer = (ServletWebRequest) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = servletContainer.getRequest();
+
+        String url = request.getRequestURL().toString();
+        long preTime = System.currentTimeMillis();
+        try {
+            Object proceed = point.proceed();
+        } catch (Throwable throwable) {
+            logger.error("拦截器异常");
+        }
+
         long afterTime = System.currentTimeMillis();
-        long time = (afterTime - preTime)/1000;
-        logger.info("访问路径为:"+url+"--------------访问时间为:"+time);
+        long diff = (afterTime - preTime)/1000;
+        logger.info("请求路径为：" + url +"----------- 耗时："+diff);
 
     }
+
 }
