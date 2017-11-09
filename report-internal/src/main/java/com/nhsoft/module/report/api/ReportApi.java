1a64cc382ad8b9ed2e1631de729e5ec707206cad
王磊 <nhsoft.wl@hotmail.com>
2017-11-09 17:04:38 +0800

    add

--- report-internal/src/main/java/com/nhsoft/module/report/api/ReportApi.java
+++ report-internal/src/main/java/com/nhsoft/module/report/api/ReportApi.java
@@ -1157,7 +1157,7 @@ public class ReportApi {
 
         List<BranchFinishRateTopDTO> newList = new ArrayList<>();
         //得到分店号,判断要不要过滤其他分店
-        if(branchNums != null && branchNums.length()>3){//查询所有分店传递额参数是"[]" lenth大于3时才会有分店传入
+        if(branchNums != null && branchNums.length()>=3){//查询所有分店传递额参数是"[]" lenth大于3时才会有分店传入
             List<Integer> branchNumList = subBranchNum(systemBookCode, branchNums);
             for (int i = 0; i <branchNumList.size() ; i++) {
                 Integer integer = branchNumList.get(i);
