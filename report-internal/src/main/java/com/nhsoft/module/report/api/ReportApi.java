9f447d3f8bbfaee265c77399552fbbc3b427e4db
王磊 <nhsoft.wl@hotmail.com>
2017-11-10 09:25:18 +0800

    add

--- report-internal/src/main/java/com/nhsoft/module/report/api/ReportApi.java
+++ report-internal/src/main/java/com/nhsoft/module/report/api/ReportApi.java
@@ -1171,7 +1171,7 @@ public class ReportApi {
             if(newList.size() == 0){
                 //如果数据被全部移出，不能返回空，因为前台要拿日期数据
                 BranchFinishRateTopDTO  branchFinishRateTopDTO = new BranchFinishRateTopDTO(date+"( 星期" + arrayDay[day] + " )");
-                list.add(branchFinishRateTopDTO);
+                newList.add(branchFinishRateTopDTO);
             }
             return newList;
         }else{
