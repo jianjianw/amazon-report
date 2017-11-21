50551a68f67f4bdd1560ba8b1ea5e6ad42af37ec
jianjianw <13992787306@163.com>
2017-11-21 21:44:51 +0800

    add

--- report-internal/src/main/java/com/nhsoft/module/report/api/APIBasic.java
+++ report-internal/src/main/java/com/nhsoft/module/report/api/APIBasic.java
@@ -40,7 +40,7 @@ public class APIBasic {
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, value = "/test/report")
-	public @ResponseBody List<BranchBizRevenueSummary> test () throws Exception {
+	public @ResponseBody List<BranchBizRevenueSummary> test () throws Exception {//不走sharding
 		String systemBookCode= "4344";
 		List<BranchDTO> all = branchRpc.findInCache(systemBookCode);
 		List<Integer> branchNums = new ArrayList<Integer>();
@@ -57,7 +57,7 @@ public class APIBasic {
 		//含inner
 		CardReportQuery cardReportQuery = new CardReportQuery();
 		cardReportQuery.setQueryDetail(true);
-		cardReportQuery.setQueryPayment(true);
+		//cardReportQuery.setQueryPayment(true);
 		cardReportQuery.setSystemBookCode("4344");
 		cardReportQuery.setBranchNum(99);
 		cardReportQuery.setDateFrom(dateFrom);
@@ -66,6 +66,34 @@ public class APIBasic {
 		System.out.println();
 		return null;
 		//return posOrderRpc.findMoneyBizdaySummary("4344", Arrays.asList(1,2,99), AppConstants.BUSINESS_TREND_PAYMENT, DateUtil.getDateStr("20170901"), DateUtil.getDateStr("20171101"), false);
+	}
+	@RequestMapping(method = RequestMethod.GET,value = "/test1")
+	@ResponseBody
+	public void test1()throws Exception{//走sharding
+		String systemBookCode= "4344";
+		List<BranchDTO> all = branchRpc.findInCache(systemBookCode);
+		List<Integer> branchNums = new ArrayList<Integer>();
+		for (BranchDTO b : all) {
+			Integer branchNum = b.getBranchNum();
+			branchNums.add(branchNum);
+		}
+		Calendar calendar = Calendar.getInstance();
+		Date date = calendar.getTime();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date dateFrom = sdf.parse("2017-10-01");
+		Date dateTo = sdf.parse("2017-10-31");
+
+		//含inner
+		CardReportQuery cardReportQuery = new CardReportQuery();
+		//cardReportQuery.setQueryDetail(true);
+		cardReportQuery.setQueryPayment(true);
+		cardReportQuery.setSystemBookCode("4344");
+		cardReportQuery.setBranchNum(99);
+		cardReportQuery.setDateFrom(dateFrom);
+		cardReportQuery.setDateTo(dateTo);
+		List<Object[]> summaryByBranch = posOrderService.findSummaryByBranch(cardReportQuery);
+		System.out.println();
+
 	}
 	
 }
