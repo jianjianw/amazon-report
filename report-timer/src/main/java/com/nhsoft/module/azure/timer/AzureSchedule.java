40c44b08137b15429673fdda5fdff358707dcc10
nhsoft.yq <nhsoft.yq@hotmail.com>
2017-12-09 21:10:47 +0800

    ALTER

--- report-timer/src/main/java/com/nhsoft/module/azure/timer/AzureSchedule.java
+++ report-timer/src/main/java/com/nhsoft/module/azure/timer/AzureSchedule.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
