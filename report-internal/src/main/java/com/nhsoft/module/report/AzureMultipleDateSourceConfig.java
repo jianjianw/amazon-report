58c1d3b20df9394d23afca71fd1988fb6eff2ead
nhsoft.yq <nhsoft.yq@hotmail.com>
2017-12-10 05:35:35 +0800

    ALTER

--- report-internal/src/main/java/com/nhsoft/module/report/AzureMultipleDateSourceConfig.java
+++ report-internal/src/main/java/com/nhsoft/module/report/AzureMultipleDateSourceConfig.java
@@ -3,13 +3,10 @@ package com.nhsoft.module.report;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
@@ -18,8 +15,8 @@ import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
-@PropertySource({"classpath:azure.properties"})
+//@Configuration
+//@PropertySource({"classpath:azure.properties"})
 public class AzureMultipleDateSourceConfig implements EnvironmentAware {
 
     private static final Logger logger = LoggerFactory.getLogger(AzureMultipleDateSourceConfig.class);
