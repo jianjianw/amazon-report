379ca11f825f3599f4804d2d0e63553023c7caee
王磊 <nhsoft.wl@hotmail.com>
2017-12-16 14:04:56 +0800

    add

--- report-internal/src/main/java/com/nhsoft/module/report/ScheduleConfig.java
+++ report-internal/src/main/java/com/nhsoft/module/report/ScheduleConfig.java
@@ -1,5 +1,7 @@
 package com.nhsoft.module.report;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,14 +12,16 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 @Configuration
-@EnableScheduling
 public class ScheduleConfig implements SchedulingConfigurer {
+
+    private static final Logger logger = LoggerFactory.getLogger(ScheduleConfig.class);
     @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
         scheduledTaskRegistrar.setScheduler(taskExecutor());
     }
     @Bean
     public Executor taskExecutor() {
+        logger.info("初始化Executor");
         return Executors.newScheduledThreadPool(100);
     }
 }
