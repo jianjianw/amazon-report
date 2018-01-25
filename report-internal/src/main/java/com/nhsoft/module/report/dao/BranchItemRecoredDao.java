5dc6f08c3210cdd88add65a32cb1345e74dec425
王磊 <nhsoft.wl@hotmail.com>
2018-01-25 16:06:47 +0800

    add

--- report-internal/src/main/java/com/nhsoft/module/report/dao/BranchItemRecoredDao.java
+++ report-internal/src/main/java/com/nhsoft/module/report/dao/BranchItemRecoredDao.java
@@ -18,6 +18,19 @@ public interface BranchItemRecoredDao {
 											List<Integer> itemNums, List<String> branchItemRecoredTypes);
 
 
+	/**
+	 * 按商品汇总首次审核日期
+	 * @param systemBookCode
+	 * @param branchNum
+	 * @param storehouseNum
+	 * @param itemNums
+	 * @param branchItemRecoredTypes
+	 * @return
+	 */
+	public List<Object[]> findItemMinAuditDate(String systemBookCode, Integer branchNum, Integer storehouseNum,
+											List<Integer> itemNums, List<String> branchItemRecoredTypes);
+
+
 
 
 }
--- report-internal/src/main/java/com/nhsoft/module/report/dao/impl/BranchItemRecoredDaoImpl.java
+++ report-internal/src/main/java/com/nhsoft/module/report/dao/impl/BranchItemRecoredDaoImpl.java
@@ -31,4 +31,24 @@ public class BranchItemRecoredDaoImpl extends DaoImpl implements BranchItemRecor
 		Query query = currentSession().createSQLQuery(sb.toString());
 		return query.list();
 	}
+
+	@Override
+	public List<Object[]> findItemMinAuditDate(String systemBookCode, Integer branchNum, Integer storehouseNum, List<Integer> itemNums, List<String> branchItemRecoredTypes) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("select item_num, min(branch_item_recored_in_date) from branch_item_recored with(nolock) ");
+		sb.append("where system_book_code = '" + systemBookCode + "' and branch_num = " + branchNum + " ");
+		if(storehouseNum != null){
+			sb.append("and storehouse_num = " + storehouseNum + " ");
+
+		}
+		if(itemNums != null && itemNums.size() > 0){
+			sb.append("and item_num in " + AppUtil.getIntegerParmeList(itemNums));
+		}
+		if(branchItemRecoredTypes != null && branchItemRecoredTypes.size() > 0){
+			sb.append("and branch_item_recored_type in " + AppUtil.getStringParmeList(branchItemRecoredTypes));
+		}
+		sb.append("group by item_num ");
+		Query query = currentSession().createSQLQuery(sb.toString());
+		return query.list();
+	}
 }
