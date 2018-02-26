b9df327f6ba4aa9aec411ca23b7b1e2d95530334
王磊 <nhsoft.wl@hotmail.com>
2018-02-26 11:56:16 +0800

    add

--- provider/src/main/java/com/nhsoft/report/provider/interceptor/ApiInterceptor.java
+++ provider/src/main/java/com/nhsoft/report/provider/interceptor/ApiInterceptor.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Aspect
 @Configuration
 public class ApiInterceptor {
@@ -39,5 +41,47 @@ public class ApiInterceptor {
             logger.info("API：" + name + "耗时：" + diff + "秒");
         }
     }
+
+
+
+    @Pointcut("execution(* com.nhsoft.module.report.dao.*.*(..))")
+    public void dao(){
+
+    }
+
+    @Around(value="ApiInterceptor.dao()")
+    public Object printDaoLog(ProceedingJoinPoint point) throws Throwable  {
+
+        String name = null;
+        Object object;
+        long diff = 0;
+        long preTime = 0;
+        Integer size = 0;
+        try {
+            name = point.getTarget().getClass().getName() + "." + point.getSignature().getName();
+            preTime = System.currentTimeMillis();
+
+            object = point.proceed(point.getArgs());
+
+            if (object instanceof List) {
+                size = ((List) object).size();
+            }
+            return object;
+        } catch (Throwable throwable) {
+            logger.error(throwable.getMessage(),throwable);
+            throw throwable;
+        } finally {
+            long afterTime = System.currentTimeMillis();
+            diff = (afterTime - preTime)/1000;
+            if(diff > 10){
+                logger.warn(String.format("接口[%s]耗时%d秒", name, diff));
+            }
+            if(size != null && size>10000){
+                logger.warn(String.format("接口[%s]返回数据%d条", name, size));
+            }
+
+        }
+    }
+
 
 }
