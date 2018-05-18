99ce981a8b8db54a4f0d7d1befddb4500703a25d
汪乾文 <nhsoft.wqw@hotmail.com>
2018-05-18 14:20:19 +0800

    农贸市场

--- report-internal/src/main/java/com/nhsoft/module/report/service/impl/ReportServiceImpl.java
+++ report-internal/src/main/java/com/nhsoft/module/report/service/impl/ReportServiceImpl.java
@@ -27,6 +27,8 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.stream.Collectors;
+
 @Service
 public class ReportServiceImpl implements ReportService {
 
@@ -9656,17 +9658,14 @@ public class ReportServiceImpl implements ReportService {
 		}
 
 		if (StringUtils.isEmpty(type)) {
-			list.addAll(reportDao.findAlipayDetailDTOs(systemBookCode, branchNums, dateFrom, dateTo, paymentTypes));
-			list.addAll(alipayLogDao.findAlipayDetailDTOs(systemBookCode, branchNums, dateFrom, dateTo, "member",
-					alipayLogTypes));
-			list.addAll(alipayLogDao.findAlipayDetailDTOs(systemBookCode, branchNums, dateFrom, dateTo, "DEP",
-					alipayLogTypes));
+			List<AlipayDetailDTO> tempList = alipayLogDao.findAlipayDetailDTOs(systemBookCode, branchNums, dateFrom, dateTo, null, alipayLogTypes).stream().filter(a -> !"微店消费".equals(a.getType())).collect(Collectors.toList());
+			list.addAll(tempList);
 			if(queryAll){
 				list.addAll(alipayLogDao.findCancelAlipayDetailDTOs(systemBookCode, branchNums, dateFrom, dateTo, null,
 						alipayLogTypes));
 			}
 		} else if (type.equals("POS消费")) {
-			list.addAll(reportDao.findAlipayDetailDTOs(systemBookCode, branchNums, dateFrom, dateTo, paymentTypes));
+			list.addAll(reportDao.findAlipayDetailDTOs(systemBookCode, branchNums, dateFrom, dateTo, paymentTypes).stream().filter("POS消费"::equals).collect(Collectors.toList()));
 			if(queryAll){
 				list.addAll(alipayLogDao.findCancelAlipayDetailDTOs(systemBookCode, branchNums, dateFrom, dateTo, "POS",
 						alipayLogTypes));
