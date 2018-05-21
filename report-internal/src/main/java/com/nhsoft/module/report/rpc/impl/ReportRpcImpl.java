2a27d9dfc9d412e1df4bd008d8887a64a8497a1b
wl <13992787306@163.com>
2018-05-21 18:16:13 +0800

    add

--- report-internal/src/main/java/com/nhsoft/module/report/rpc/impl/ReportRpcImpl.java
+++ report-internal/src/main/java/com/nhsoft/module/report/rpc/impl/ReportRpcImpl.java
@@ -5390,7 +5390,7 @@ public class ReportRpcImpl implements ReportRpc {
 		//批发数量
 		List<WholesaleAmountAndMoneyDTO> wholesaleSum = wholesaleOrderRpc.findAmountAndMoneyByBiz(systemBookCode, dateFrom, dateTo, itemNums);
 		//库存  当前库存
-        List<Inventory> inventories = inventoryService.findByItemAndBranch(systemBookCode, 99, itemNums, true);
+        List<Inventory> inventories = inventoryService.findByItemAndBranch(systemBookCode, 99, itemNums, null);
 		PosItemQuery query = new PosItemQuery();
 		query.setSystemBookCode(systemBookCode);
 		query.setItemNums(itemNums);
